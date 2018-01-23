8db1da4b07d78d092754fcba784973159b6c6129
Albert Ngoudjou <angoudjou@yahoo.fr>
2018-01-23 14:39:23 -0500

    lab2 first commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int number;
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine();
+            bool continue_run = true;
+            while (continue_run)
+            {
+                Console.WriteLine();
+                Console.Write("Enter a number between 1 and 100 : ");
+                number = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                if (number >= 1 && number <= 100)
+                {
+                    if (number % 2 == 1 && number<=60)
+                    {
+                        Console.WriteLine("The number you entered is odd");
+                    }
+
+                    else 
+                    if (number % 2 == 1 && number > 60)
+                    {
+                        Console.WriteLine("The number you entered is Odd");
+                    }
+                    else
+                    if (number % 2 == 0)
+                    {
+                        if (number >= 2 && number <= 25)
+                        {
+                            Console.WriteLine("The number is even and less than 25");
+                        }
+                        else
+                        if (number >= 26 && number <= 60)
+                        {
+                            Console.WriteLine("The number entered is even");
+                        }
+                        else
+                    if (number > 60)
+                        {
+                            Console.WriteLine("The number entered is Even");
+                        }
+                    }
+                    
+            }
+            else
+            {
+                Console.WriteLine("hey " + name + " Your entered a number out range 1-100");
+             //   Console.ReadKey();
+
+            }
+
+            Console.Write("continue(Y/N)");
+            continue_run = Console.ReadKey().Key == ConsoleKey.Y;
+        }
+            //end while loop
+         //   Console.ReadKey();
+
+        }
+    }
+}
